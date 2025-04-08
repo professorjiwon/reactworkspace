944711402ee68628004473f153134d6b8e6605b0
professorjiwon <professorjiwon@gmail.com>
2025-04-08 12:19:02 +0900

    04.08_3교시

--- 09.props/src/App.js
+++ 09.props/src/App.js
@@ -9,14 +9,27 @@
 import './App.css';
 import Compo from './components/Compo';
 
+/*  1. 문자 혹은 변수로 값 넘겨주기
 function App() {
   const addr = "서초구 강남대로";
-  
+
   return (
     <div className="App">
       <Compo user={"홍길동"} addr={addr}/>
     </div>
   );
+}
+*/ 
+
+
+function App() {
+  const userInfo = {
+    name : "임수정",
+    addr : "경기도 광주시",
+    likeList : ['캐릭터', '만화', '웹툰']
+  }
+  // return <Compo user={userInfo} />   //  2. 객체로 넘겨주기
+  return <Compo {...userInfo} />        // 3. 객체를 스프레드 연사자로 풀어서 넘겨주기
 }
 
 export default App;
--- 09.props/src/components/Compo.js
+++ 09.props/src/components/Compo.js
@@ -1,3 +1,4 @@
+/*  1. 문자 혹은 변수로 값 받기 
 const Compo = (props) => {
     return (
         <>
@@ -7,5 +8,32 @@ const Compo = (props) => {
         </>
     )
 }
+*/
 
+/* 2. 객체로 넘겨준값 받기
+const Compo = (props) => {
+    return (
+        <>
+            <h3>props 내용 출력</h3>
+            <p>이름 : {props.user.name}</p>
+            <p>주소 : {props.user.addr}</p>
+            <p>특히 좋아하는 것 : {props.user.likeList[2]}</p>
+        </>
+    )
+}
+*/
+
+
+// 3. 객체를 스프레드 연사자로 풀어서 넘겨준 값 받기
+const Compo = ({name, addr, likeList}) => {
+    return (
+        <>
+            <h3>props 내용 출력</h3>
+            <p>이름 : {name}</p>
+            <p>주소 : {addr}</p>
+            <p>특히 좋아하는 것 : {likeList[2]}</p>
+        </>
+    )
+    
+}
 export default Compo;
