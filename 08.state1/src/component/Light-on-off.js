837be58de74dfba250105740f23015ece4d58663
professorjiwon <professorjiwon@gmail.com>
2025-04-08 09:27:42 +0900

    04.07_8교시

---
+++ 08.state1/src/component/Light-on-off.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const LightOnOff = () => {
+    // light = true, false
+    const [light, setLight] = useState(false);
+    const [colorSt, setColorSt] = useState();
+    return (
+        <>
+            { light ? <h1 style={{color:'hotpink'}}>전구ON</h1> : <h1 style={{color:'gray'}}>전구OFF</h1>}
+            <button onClick={()=>{setLight(!light)}}>
+                {light ? "끄기" : "켜기"}
+            </button>
+        </>
+    )
+
+}
+
+export default LightOnOff;
---
+++ 09.state2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Views from './component/View.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <Views count/>  
+    </div>
+  );
+}
+
+export default App;
