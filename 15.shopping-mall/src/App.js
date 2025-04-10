07bba07567c990c1c7dfd16e8e404fa225117c04
professorjiwon <professorjiwon@gmail.com>
2025-04-10 15:40:47 +0900

    04.10_5교시

--- 15.shopping-mall/src/App.js
+++ 15.shopping-mall/src/App.js
@@ -3,7 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
 import pList from './data/ProductList';
+import { Route, Routes, Link } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Detail from './pages/Detail';
 
+/*
+  * react-router-dom
+    : 페이지를 교체시켜주는 api -> router-dom
+
+  * 사용하려면
+    1. 설치 : npm i react-router-dom
+    2. index.js에 <BrowserRouter> 태그 넣어주기
+*/
 function App() {
   const [clothes, setClothes] = useState(pList);
 
@@ -19,41 +30,43 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'/> 
+      <Link to="/">Home</Link>
+      <Link to="/detail">상세페이지</Link>
+      <Link to="/cart">장바구니</Link>
 
-      <Container>
-        <Row>
-          <Col>
-            <img src = {`${process.env.PUBLIC_URL}/img/clothes1.png`} width="75%"/>
-            <h4>{clothes[0].title}</h4>
-            <p>{clothes[0].price}</p>
-          </Col>
-          <Col>
-            <img src = {`${process.env.PUBLIC_URL}/img/clothes2.png`} width="75%"/>
-            <h4>{clothes[1].title}</h4>
-            <p>{clothes[1].price}</p>
-          </Col>
-          <Col>
-            <img src = {`${process.env.PUBLIC_URL}/img/clothes3.png`} width="75%"/>
-            <h4>{clothes[2].title}</h4>
-            <p>{clothes[2].price}</p>
-          </Col>
-        </Row>
-      </Container>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className='main-bg'/>     
+            <Container>
+              <Row>
+                {
+                  clothes.map((v, i) => {
+                      return (
+                        <PListCol clothes={v} key={i}/>
+                      )
+                  })
+                }
+              </Row>
+            </Container>
+          </>
+        }/>
+        <Route path='/detail' element={<Detail/>} />
+        <Route path='/cart' element={<Cart/>} />
+      </Routes>
     </div>
   );
 }
 
-function PListCol() {
+function PListCol(props) {
   return (
     <>
       <Col>
-        <img src = {`${process.env.PUBLIC_URL}/img/clothes1.png`} width="75%"/>
-        <h4>상품명</h4>
-        <p>가격</p>
+        <img src = {`${process.env.PUBLIC_URL}/img/clothes${props.clothes.id}.png`} width="75%"/>
+        <h4>{props.clothes.title}</h4>
+        <p>{props.clothes.price}</p>
       </Col>
     </>
   )
 }
-
 export default App;
